f5da34cf3f1330dd9ccbd75997dea087bd42bd9f
lipingxie <guxieliping@hotmail.com>
2021-07-26 22:30:36 +1000

    update

---
+++ data/cleandata.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from collections import defaultdict
+
+
+dtypes = {
+    "lga_name19": "category",
+    "SType": "category"
+}
+
+df = pd.read_csv(
+    "confirmed_cases_table1_location.csv",
+    dtype=dtypes,
+    usecols=list(dtypes) + ["notification_date", "TY"],
+    parse_dates=["notification_date"]
+)
+
+nd = df.groupby("notification_date").size()
+df1 = nd.reset_index()
+df1.columns =['notification_date', 'TY']
+df1["SType"]="All"
+
+lga_list = df['lga_name19'].unique()
+
+for lga in lga_list:
+    df2 = df[df['lga_name19'] == lga]
+    df2 = df2.groupby("notification_date").size()
+    df2 = df2.reset_index()
+    df2.columns =['notification_date', 'TY']
+    df2["SType"]=lga
+    df1 = df1.append(df2)
+
+df1.to_csv('confirmed_cases_table1_location_result.csv', index=True, encoding='utf-8')
+
